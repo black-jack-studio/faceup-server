460f8812e295cdef8a8c7b2251101c66503d140f
stanislas1418 <46953612-stanislas1418@users.noreply.replit.com>
2025-10-02 07:38:54 +0000

    Restored to '4ac13a830b82fbed1f844ea7d1b02e28634fc2ee'
    Replit-Restored-To: 4ac13a830b82fbed1f844ea7d1b02e28634fc2ee

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.faceup.app',
-  appName: 'FaceUp',
-  webDir: 'dist'
-};
-
-export default config;
